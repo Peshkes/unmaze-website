cf427375c3068cb44e8f5b3f2d833b2b2f5752f5
peshkes <peshkesv@gmail.com>
2024-07-19 15:31:36 +0400

    Added images

--- src/libraries/i18next/ru.js
+++ src/libraries/i18next/ru.js
@@ -1,3 +1,13 @@
-const ru = {};
+const ru = {
+    header:{
+        aboutUs: "О нас",
+        dyslexia: "Диcлексия",
+        test: "Тест",
+        guide: "Гайд",
+        questions: "Вопросы",
+        articles: "Статьи",
+        writeToUs: "Напишите нам"
+    }
+};
 
 export default ru;
