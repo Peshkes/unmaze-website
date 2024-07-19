941775dbf3fefab95c4ca0dd2d0ccdf13115e903
peshkes <peshkesv@gmail.com>
2024-07-19 15:33:27 +0400

    Added helloScreen

--- src/components/blocks/hello-screen/HelloScreen.tsx
+++ src/components/blocks/hello-screen/HelloScreen.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import style from './description.module.css'
+import style from './helloScreen.module.css'
 
 const HelloScreen = () => {
     return (
-        <div>
-
+        <div className={style.block}>
+            <h1 className={'wrapper'}>привет, мы  unmaze — приложение, которое помогает детям
+                с дислексией</h1>
+            <div className={style.imageBlock}>
+                <img src={require('../../../assets/iphone.webp')} alt="App Screen"/>
+                <img className={style.mess} src={require('../../../assets/mess1.webp')} alt="App Screen"/>
+            </div>
+            <div className={"button"}>
+                <a href="#">Скачать приложение<span className="arrow">→</span></a>
+            </div>
         </div>
     );
 };
